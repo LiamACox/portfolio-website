fcb2e155f3e3e6fd9a07bd96f7286937f4f73b0c
LiamACox <liamanthonycox@gmail.com>
2024-09-12 13:35:32 -0400

    Added logos and links. Corrected placement of Navbar components.

--- components/background/background.tsx
+++ components/background/background.tsx
@@ -4,7 +4,7 @@ export default function Background() {
     return (
         <div className="fixed -z-1">
             <Starfield
-                starCount={2500}
+                starCount={1000}
                 starColor={[255, 255, 255]}
                 speedFactor={0.01}
                 backgroundColor="black"
--- components/navbar/navbar.tsx
+++ components/navbar/navbar.tsx
@@ -1,17 +1,32 @@
 import Link from 'next/link';
+import Image from 'next/image';
+
+import LinkedInIcon from '@/public/In-White-128.png';
+import GithubIcon from '@/public/github-mark-white.svg';
+import EmailIcon from '@/public/email.svg';
 
 export default function Navbar() {
-    return (<nav className="fixed z-1 top-0 left-0 p-5 w-full flex items-center justify-between">
-        <nav className="flex-1 text-center justify-between space-x-20">
-            <Link href="/about" className="text-4xl hover:underline">About</Link>
-            <Link href="/projects" className="text-4xl hover:underline">Projects</Link>
-            <Link href="/about" className="text-4xl hover:underline">Resume</Link>
+    return (
+        <nav className="fixed z-1 top-0 left-0 p-5 w-full flex items-center justify-between">
+            <div className="flex-1 text-center space-x-20">
+                <Link href="/about" className="hover:underline">About</Link>
+                <Link href="/projects" className="hover:underline">Projects</Link>
+                <Link href="/about" className="hover:underline">Resume</Link>
+            </div>
+            <div className="flex-1 text-center">
+                <Link href="/" className="hover:underline">LIAMC.XYZ</Link>
+            </div>
+            <div className="flex-1 flex place-content-center space-x-20">
+                <Link href={'https://linkedin.com'} target="_blank" rel="noopener noreferrer">
+                    <Image src={LinkedInIcon} alt="My LinkedIn" width={64} height={64}/>
+                </Link>
+                <Link href={'https://github.com'} target="_blank" rel="noopener noreferrer">
+                    <Image src={GithubIcon} alt="My Github" width={64} height={64}/>
+                </Link>
+                <Link href={'mailto:coxx9803@mylaurier.ca'} className="invert">
+                    <Image src={EmailIcon} alt="Email me!" width={64} height={64}/>
+                </Link>
+            </div>
         </nav>
-        <nav className="flex-1 text-center">
-            <Link href="/" className="text-8xl hover:underline">LIAMC.XYZ</Link>
-        </nav>
-        <nav className="flex-1 text-right">
-            <Link href="/" className="text-4xl border-2 border-white rounded-lg p-4 space-x-4 hover:underline">CONTACT</Link>
-        </nav>
-    </nav>);
+    );
 }
