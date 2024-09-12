abd21781ca0ccee0c20c52ffae0f2dde1069a88a
LiamACox <liamanthonycox@gmail.com>
2024-09-11 21:56:47 -0400

    Added 'z-index: -1' to Background component to ensure correct layering

--- components/background/background.tsx
+++ components/background/background.tsx
@@ -2,11 +2,13 @@ import Starfield from 'react-starfield';
 
 export default function Background() {
     return (
-        <Starfield
-            starCount={2500}
-            starColor={[255, 255, 255]}
-            speedFactor={0.01}
-            backgroundColor="black"
-        />
+        <div className="fixed -z-1">
+            <Starfield
+                starCount={2500}
+                starColor={[255, 255, 255]}
+                speedFactor={0.01}
+                backgroundColor="black"
+            />
+        </div>
     );
 }
